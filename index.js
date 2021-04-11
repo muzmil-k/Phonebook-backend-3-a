9c83ef3de275f541c4d0f7d317d45a35d78915c7
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-04-11 22:37:48 +0500

    phonebook backend 7-8

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const e = require('express');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 app.use(express.json())
+morgan.token('body', function (request, response) { return JSON.stringify(request.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
     {
